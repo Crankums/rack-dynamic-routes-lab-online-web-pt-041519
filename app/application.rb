3cdfc1dc8f2411308f40c7f4e53740839a273ce2
Craig Donavin <crdonavin@gmail.com>
2019-07-18 18:35:55 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,12 +1,36 @@
+# class Application
+#   def call(env)
+#     resp = Rack::Response.new
+#     req = Rack::Request.new(env)
+# 
+#     if req.path.match(/items/)
+#       item_name = req.path.split("/items/").last
+#       if item = @@items.find{|i| i.name == item_name} #why is this not a '=='?
+#         resp.write item.price
+#       else
+#         resp.write "Item not found"
+#         resp.status = 400
+#       end
+#     else
+#       resp.write "Route not found"
+#       resp.status = 404
+#     end
+# 
+#     resp.finish
+#   end
+# end
+
 class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path.match(/items/)
-      item_name = req.path.split("/items/").last
-      if item = @@items.find{|i| i.name == item_name} #why is this not a '=='?
-        resp.write item.price
+    if req.path.include?("/items/")
+      requested_item = req.path.split("/items/").last
+      found_item = Item.items.find {|i| i.name == requested_item }
+      # binding.pry
+      if !found_item.nil?
+        resp.write "#{found_item.price}"
       else
         resp.write "Item not found"
         resp.status = 400
@@ -15,7 +39,5 @@ class Application
       resp.write "Route not found"
       resp.status = 404
     end
-
-    resp.finish
   end
 end
