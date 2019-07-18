4107f14c898131c939a57b2c2c046fa418165bf0
Craig Donavin <crdonavin@gmail.com>
2019-07-18 18:30:16 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
 
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
-      if item = @@items.find{|i| i.name == item_name}
+      if item = @@items.find{|i| i.name == item_name} #why is this not a '=='?
         resp.write item.price
       else
         resp.write "Item not found"
