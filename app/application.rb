aa66457a2688ac86713de4d895981786c47902d6
Craig Donavin <crdonavin@gmail.com>
2019-07-18 18:41:15 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,35 +1,13 @@
-# class Application
-#   def call(env)
-#     resp = Rack::Response.new
-#     req = Rack::Request.new(env)
-#
-#     if req.path.match(/items/)
-#       item_name = req.path.split("/items/").last
-#       if item = @@items.find{|i| i.name == item_name} #why is this not a '=='?
-#         resp.write item.price
-#       else
-#         resp.write "Item not found"
-#         resp.status = 400
-#       end
-#     else
-#       resp.write "Route not found"
-#       resp.status = 404
-#     end
-#
-#     resp.finish
-#   end
-# end
-
 class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
-      requested_item = req.path.split("/items/").last
-      if @@items.find {|i| i.name == requested_item }
-      if !found_item.nil?
-        resp.write "#{found_item.price}"
+      item_name = req.path.split("/items/").last
+      item = @@items.find{|i| i.name == item_name} #why is this not a '=='?
+      if item != nil
+        resp.write item.price
       else
         resp.write "Item not found"
         resp.status = 400
@@ -38,5 +16,7 @@ class Application
       resp.write "Route not found"
       resp.status = 404
     end
+
+    resp.finish
   end
 end
