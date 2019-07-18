1a41c99d7c22f5dff63f910d3b4709599bf69fd7
Craig Donavin <crdonavin@gmail.com>
2019-07-18 18:42:52 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,7 @@
 #   def call(env)
 #     resp = Rack::Response.new
 #     req = Rack::Request.new(env)
-# 
+#
 #     if req.path.match(/items/)
 #       item_name = req.path.split("/items/").last
 #       item = @@items.find{|i| i.name == item_name} #why is this not a '=='?
