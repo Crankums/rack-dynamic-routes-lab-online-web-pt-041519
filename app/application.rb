2800641989f88dcdffa7c4a0e51c0278eeb6d641
Craig Donavin <crdonavin@gmail.com>
2019-07-18 18:43:05 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -26,7 +26,7 @@ class Application
 
     if req.path.include?("/items/")
       requested_item = req.path.split("/items/").last
-      found_item = Item.items.find {|i| i.name == requested_item }
+      found_item = @@items.find {|i| i.name == requested_item }
       # binding.pry
       if !found_item.nil?
         resp.write "#{found_item.price}"
