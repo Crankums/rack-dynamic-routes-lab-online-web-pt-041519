c04f84421eae1ec15f862e34079dd29f07576e1e
Craig Donavin <crdonavin@gmail.com>
2019-07-18 18:45:22 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -29,7 +29,7 @@ class Application
       found_item = Item.items.find {|i| i.name == requested_item }
       # binding.pry
       if found_item != nil
-        resp.write "#{found_item.price}"
+        resp.write found_item.price
       else
         resp.write "Item not found"
         resp.status = 400
